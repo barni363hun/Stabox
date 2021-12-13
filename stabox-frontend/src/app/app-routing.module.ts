2c12391d048c0ffdbee22393822080b309eecd16
tomy002 <tamasattila02@gmail.com>
2021-12-13 12:17:23 +0100

    background

--- stabox-frontend/src/app/app-routing.module.ts
+++ stabox-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
