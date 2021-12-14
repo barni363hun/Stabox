9f2be61f613f8b50b66baf01d2955311f36cb3bf
tomy002 <tamasattila02@gmail.com>
2021-12-14 08:25:28 +0100

    custom scrollbar

--- stabox-frontend/src/app/app.module.ts
+++ stabox-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './components/mainpage/mainpage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
