3f2099a6d57b6a018a95e2fba4152e3b57ec17fc
Princzes Barnabás <barni.pbs@gmail.com>
2022-01-16 18:54:10 +0100

    auth buttons

--- stabox-frontend/src/app/components/buttons/sign-up-button/sign-up-button.component.ts
+++ stabox-frontend/src/app/components/buttons/sign-up-button/sign-up-button.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up-button',
   templateUrl: './sign-up-button.component.html',
-  styleUrls: ['./sign-up-button.component.scss']
+  styleUrls: ['./sign-up-button.component.scss'],
 })
 export class SignUpButtonComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
