6d2f8518787d33a1609cf4a19ebd57493aed68fb
Princzes Barnabás <barni.pbs@gmail.com>
2022-01-27 09:41:23 +0100

    roles done

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -4,20 +4,22 @@ import { Message } from '@stabox/stabox-lib';
 
 import { AppService } from './app.service';
 import { AuthGuard } from './auth/auth.guard';
-import { PermissionsGuard } from './auth/permissions.guard';
+import { RoleGuard } from './auth/role.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  //needs to be logged in
   @UseGuards(AuthGuard)
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
 
-  @UseGuards(AuthGuard, PermissionsGuard)
-  @SetMetadata('permissions', ['read:cats'])
+  //needs admin role
+  @UseGuards(AuthGuard, RoleGuard)
+  @SetMetadata('roles', ['admin'])
   @Get('/cat')
   getCat(): string {
     return this.appService.getCat();
