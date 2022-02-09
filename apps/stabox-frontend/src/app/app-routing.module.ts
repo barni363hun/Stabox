95ed3d13e712df44d547f7d9db3997bd28b5218e
Princzes Barnabás <barni.pbs@gmail.com>
2022-02-09 14:17:03 +0100

    login logout

--- apps/stabox-frontend/src/app/app-routing.module.ts
+++ apps/stabox-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { AccountPageComponent } from './components/pages/account-page/account-page.component';
 import { MainpageComponent } from './components/pages/mainpage/mainpage.component';
 import { MyPackagesPageComponent } from './components/pages/my-packages-page/my-packages-page.component';
 
 const routes: Routes = [
-  { path: '', component: MainpageComponent },
-  { path: 'account', component: AccountPageComponent },
-  { path: 'mypackages', component: MyPackagesPageComponent }
+  { path: '', component: MainpageComponent, pathMatch: 'full' },
+  {
+    path: 'account',
+    component: AccountPageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'packages',
+    component: MyPackagesPageComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: '**', component: MainpageComponent },
 ];
 
 @NgModule({
