169d8b50f5efb47a690221ab3bff72da115a272b
tomy002 <tamasattila02@gmail.com>
2022-02-18 10:41:47 +0100

    account page fixes, added a save button

--- apps/stabox-frontend/src/app/components/pages/account-page/account-page.component.ts
+++ apps/stabox-frontend/src/app/components/pages/account-page/account-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPageComponent implements OnInit {
 
+  userDataChanged = true;
   viewDetails = false;
   lightMode = false;
   theme = "dark"
