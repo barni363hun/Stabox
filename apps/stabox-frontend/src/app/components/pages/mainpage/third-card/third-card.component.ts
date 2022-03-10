4ae181ef6358d9dc0b789c5ad6e65d24e77225b7
tomy002 <tamasattila02@gmail.com>
2022-03-10 12:50:52 +0100

    main page lightmode support

--- apps/stabox-frontend/src/app/components/pages/mainpage/third-card/third-card.component.ts
+++ apps/stabox-frontend/src/app/components/pages/mainpage/third-card/third-card.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'apps/stabox-frontend/src/app/services';
 
 @Component({
   selector: 'app-third-card',
   templateUrl: './third-card.component.html',
-  styleUrls: ['./third-card.component.scss']
+  styleUrls: ['./third-card.component.scss'],
 })
 export class ThirdCardComponent implements OnInit {
+  constructor(public themeService: ThemeService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
