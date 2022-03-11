06f3d0c665acd372a3e851b95c8a8f3c3db98f37
barni363hun <barni.pbs@gmail.com>
2022-03-11 13:03:39 +0100

    package range fixed

--- apps/stabox-frontend/src/app/components/package/package-card/package-card.component.ts
+++ apps/stabox-frontend/src/app/components/package/package-card/package-card.component.ts
@@ -1,58 +1,81 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ExchangeDateService, UserService, VehicleService } from '../../../services';
+import { cError, cLog } from '@stabox/stabox-lib';
+import {
+  ExchangeDateService,
+  SnackbarService,
+  UserService,
+  VehicleService,
+} from '../../../services';
 import { PackageService } from '../../../services/package.service';
 @Component({
   selector: 'stabox-package-card',
   templateUrl: './package-card.component.html',
-  styleUrls: ['./package-card.component.scss']
+  styleUrls: ['./package-card.component.scss'],
 })
-
 export class PackageCardComponent implements OnInit {
   @Input() package!: any;
   isAvailable: boolean = false;
   state: 'notShipper' | 'finished' | 'shippable' | 'finishable' = 'finished';
-  status: 'waiting for shipper' | 'being shipped' | 'finished' = 'waiting for shipper';
+  status: 'waiting for shipper' | 'being shipped' | 'finished' =
+    'waiting for shipper';
 
   isUserTheOwner: boolean = false;
 
-  vehicles: any[] = []
+  vehicles: any[] = [];
   selectedVehicle: number = 0;
 
-  exchangeDates:{startDate:Date, endDate:Date}[]| any[] = [];
+  exchangeDates: { startDate: Date; endDate: Date }[] = [];
   selectedExchangeDate: Date = new Date();
 
   constructor(
     private userService: UserService,
     private packageService: PackageService,
     private vehicleService: VehicleService,
-    private exchangeDateSercice: ExchangeDateService
-  ) { }
+    private exchangeDateSercice: ExchangeDateService,
+    private snackbarService: SnackbarService
+  ) {}
 
   ngOnInit(): void {
+    this.vehicles = this.vehicleService.vehicles;
+    this.exchangeDateSercice
+      .getExchangeDateByPackageId(this.package.fromAddressId)
+      .subscribe({
+        next: (res) => {
+          this.exchangeDates = res;
+        },
+        error: (err) => console.log(err),
+      });
 
-    this.vehicles = this.vehicleService.vehicles
-    this.exchangeDateSercice.getExchangeDateByPackageId(this.package.fromAddressId).subscribe({
-      next: (res) => {
-        this.exchangeDates = res
-      },
-      error: (err) => console.log(err),
-    })
-
-    this.updateState()
+    this.updateState();
     this.isUserTheOwner = this.userService.user.id == this.package.userId;
-    this.status = this.package.vehicleId == null ? 'waiting for shipper' : this.package.shippingDate ? 'finished' : 'being shipped'
+    this.status =
+      this.package.vehicleId == null
+        ? 'waiting for shipper'
+        : this.package.shippingDate
+        ? 'finished'
+        : 'being shipped';
   }
 
   selectPostDate() {
-    let date = this.selectedExchangeDate
+    const date: Date = this.selectedExchangeDate;
     if (!this.selectedVehicle) {
-      console.log('NO vehicle selected')
-    }
-    else if (!this.selectedExchangeDate) {
-      console.log('No exchangeDate selected')
-    }
-    else {
-      this.packageService.postPackage(this.package.id, Number(this.selectedVehicle), date);
+      cError('NO vehicle selected!');
+      this.snackbarService.showErrorSnackbar('NO vehicle selected!');
+    } else if (!this.selectedExchangeDate) {
+      cError('No exchangeDate selected!');
+      this.snackbarService.showErrorSnackbar('NO exchangeDate selected!');
+    } else if (!this.isInDateRanges(date)) {
+      cError('exchangeDate Is Not In Range!');
+      this.snackbarService.showErrorSnackbar('exchangeDate Is Not In Range!');
+    } else {
+      this.packageService.postPackage(
+        this.package.id,
+        Number(this.selectedVehicle),
+        date
+      );
+      this.snackbarService.showSuccessSnackbar(
+        'Package selected successfully!'
+      );
     }
     this.updateState();
   }
@@ -60,20 +83,42 @@ export class PackageCardComponent implements OnInit {
   finishShipping() {
     this.packageService.finishPackage(this.package.id);
     this.updateState();
+  }
 
+  isInDateRanges(d: any): boolean {
+    let back = false;
+    const myDate = new Date(d);
+    this.exchangeDates.forEach((range) => {
+      if (
+        myDate.getTime() > new Date(range.startDate).getTime() &&
+        myDate.getTime() < new Date(range.endDate).getTime()
+      ) {
+        back = true;
+      }
+    });
+    return back;
+  }
+
+  //TODO a d valamiért string-ként jön de mégse
+  formatDate(sDate: any): string {
+    const d = new Date(sDate);
+    const dformat =
+      [d.getFullYear(), d.getMonth() + 1, d.getDate()].join('-') +
+      ' ' +
+      [d.getHours(), d.getMinutes()].join(':');
+    return dformat;
   }
 
   updateState() {
-    if (!this.userService.user['https://www.stabox.hu/roles'].includes('shipper')) {
+    if (
+      !this.userService.user['https://www.stabox.hu/roles'].includes('shipper')
+    ) {
       this.state = 'notShipper';
-    }
-    else if (!this.package.vehicleId) {
+    } else if (!this.package.vehicleId) {
       this.state = 'shippable';
-    }
-    else if (!this.package.shippingDate) {
+    } else if (!this.package.shippingDate) {
       this.state = 'finishable';
-    }
-    else {
+    } else {
       this.state = 'finished';
     }
   }
