26c5c80b4f3a36766a37725f18e84175824ea52e
tomy002 <tamasattila02@gmail.com>
2022-03-23 13:37:18 +0100

    moved style from index.html to app.component

--- apps/stabox-frontend/src/app/app.component.ts
+++ apps/stabox-frontend/src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './services';
 
 @Component({
   selector: 'stabox-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(public themeService: ThemeService) {}
+
+  ngOnInit(): void {}
+}
