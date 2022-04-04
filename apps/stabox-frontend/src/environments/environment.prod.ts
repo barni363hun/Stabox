780cb19dab56fe3943ecfbd1bd035cd167573082
barni363hun <barni.pbs@gmail.com>
2022-04-04 11:01:45 +0200

    proxy to dev mode

--- apps/stabox-frontend/src/environments/environment.prod.ts
+++ apps/stabox-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:8080/api',
+  apiURL: 'http://localhost/api',
   auth: {
     domain: 'barni363hun.eu.auth0.com',
     clientId: '70x759xfYo7pvQS39ptmBpnpBRv8MUkA',
