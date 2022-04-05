fa55a0f66b3b90f11a9ddca9bfde1e3c595a6ffc
tomy002 <tamasattila02@gmail.com>
2022-04-05 10:17:49 +0200

    fixed imports in components/old/*

--- apps/stabox-frontend/src/app/components/old/pages/mainpage/first-card/first-card.component.ts
+++ apps/stabox-frontend/src/app/components/old/pages/mainpage/first-card/first-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from 'apps/stabox-frontend/src/app/services';
 
 @Component({
--- apps/stabox-frontend/src/app/components/old/pages/mainpage/third-card/third-card.component.ts
+++ apps/stabox-frontend/src/app/components/old/pages/mainpage/third-card/third-card.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { ApplicationRef, ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
-import { SnackbarService, ThemeService } from 'apps/stabox-frontend/src/app/services';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'apps/stabox-frontend/src/app/services';
 import { ContactusService } from 'apps/stabox-frontend/src/app/services/contactus.service';
-import { environment } from 'apps/stabox-frontend/src/environments/environment';
 
 @Component({
   selector: 'app-third-card',
@@ -10,12 +8,13 @@ import { environment } from 'apps/stabox-frontend/src/environments/environment';
   styleUrls: ['./third-card.component.scss'],
 })
 export class ThirdCardComponent implements OnInit {
-  constructor(private contactService:ContactusService,
-    private snackbarService: SnackbarService, public themeService: ThemeService) { }
+  constructor(
+    private contactService: ContactusService,
+    public themeService: ThemeService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   sendFeedback() {
-  this.contactService.sendFeedback()
+    this.contactService.sendFeedback();
   }
 }
